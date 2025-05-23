c362097db6f1a2533c8d212fdee4feadb92ec832
Maciej Sieradz <macieks53@gmail.com>
2025-05-23 15:27:06 +0200

    Add math for creating hand of cards

--- scripts/deck.gd
+++ scripts/deck.gd
@@ -3,13 +3,14 @@ extends Node2D
 @onready var attack_card_scene: PackedScene = preload("res://scenes/cards/attack_card.tscn")
 @onready var defend_card_scene: PackedScene = preload("res://scenes/cards/defend_card.tscn")
 
+@onready var hand: Hand = $CanvasLayer/Hand
 @onready var spawn_point = $CanvasLayer/Spawn
 
 func _on_button_pressed() -> void:
 	var attack_card = attack_card_scene.instantiate()
-	spawn_point.add_child(attack_card)
+	hand.add_card(attack_card)
 
 
 func _on_button_2_pressed() -> void:
 	var defend_card = defend_card_scene.instantiate()
-	spawn_point.add_child(defend_card)
+	hand.add_card(defend_card)
--- scripts/hand.gd
+++ scripts/hand.gd
@@ -1,11 +1,27 @@
 @tool
-extends Node2D
+class_name Hand extends Node2D
 
 @export var hand_radius: int = 100
 @export var card_angle: float = PI / 2
+@export var angle_limit: float = PI / 24
+@export var max_card_spread_angle: float = PI / 24
 
 @onready var test_card = $TestCard
 @onready var collision_shape: CollisionShape2D = $DebugShape
+
+var hand: Array = []
+
+func add_card(card: Node2D):
+	hand.push_back(card)
+	add_child(card)
+	reposition_cards()
+	
+func reposition_cards():
+	var card_spread = min(angle_limit, max_card_spread_angle) / hand.size()
+	var current_angle = -(card_spread * (hand.size() - 1))/2 - PI / 2
+	for card in hand:
+		_update_card_transform(card, current_angle)
+		current_angle += card_spread
 
 # IMPORTANT: angle is in radians
 func get_card_position(angle: float) -> Vector2:
@@ -13,8 +29,13 @@ func get_card_position(angle: float) -> Vector2:
 	var y: float = hand_radius * sin(angle)
 	
 	return Vector2(x, y)
-		
+
+func _update_card_transform(card: Node2D, angle: float) -> void:
+	card.set_position(get_card_position(angle))
+	card.set_rotation(angle + PI / 2)
+
 func _process(delta: float) -> void:
+	
 	if (collision_shape.shape as CircleShape2D).radius != hand_radius:
 		(collision_shape.shape as CircleShape2D).set_radius(hand_radius)
 
