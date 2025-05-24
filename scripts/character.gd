95e0e736780b3da56f4827f5f865f9617d931892
Maciej Sieradz <macieks53@gmail.com>
2025-05-24 17:03:32 +0200

    Fix the bugs in dmg/health methods

--- scripts/character.gd
+++ scripts/character.gd
@@ -4,6 +4,7 @@ class_name Character extends Node2D
 @export var max_health: int = 10
 @export var health: int = 10
 @export var mana: int = 10
+@export var armor: int = 0
 
 func set_health_values(health: int, max_health: int):
 	self.health = health
@@ -11,10 +12,13 @@ func set_health_values(health: int, max_health: int):
 	update_health_bar()
 	
 func spend_mana(amount: int) -> void:
-	mana -= 1
+	mana -= amount
 	
 func take_damage(amount: int) -> void:
-	health -= 1
+	health -= amount
+
+func add_armor(amount: int) -> void:
+	armor += amount
 	
 func update_health_bar():
 	if ($HealthBar as ProgressBar).max_value != max_health:
