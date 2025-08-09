ecfb1de7b5019ad27730336c56fef4a8d369c4a0
Maciej Sieradz <macieks53@gmail.com>
2025-08-09 14:27:03 +0200

    added battle_events and refactored logic for battle_manager

--- utils/managers/battle_manager/battle_manager.gd
+++ utils/managers/battle_manager/battle_manager.gd
@@ -1,7 +1,7 @@
+"""
+BattleManager class is a class required for every battle scene. It contains the state and logic for a battle. For altering the state use signals from battle_events.
+"""
 class_name BattleManager extends Node
-
-signal turn_ended()
-signal battle_ended()
 
 # To be defined
 enum BattleState {
@@ -17,24 +17,16 @@ enum BattleState {
 var current_state: BattleState = BattleState.BATTLE_SETUP
 
 # To be discussed
-# @onready var player_character = $PlayerCharacter
-# @onready var enemies = $Enemies
+@export var player_character: Node2D
+@export var enemies: Node2D
 
 func _ready() -> void:
 	print("Battle started! Initializing...")
-
-	turn_ended.connect(_on_turn_ended)
+	var battle_events: BattleEvents = get_tree().get_first_node_in_group("battle_events")
+	if battle_events:
+		battle_events.end_turn.connect(_on_turn_ended)
 
 	_transition_to_state(BattleState.BATTLE_SETUP)
-
-func _process(_delta: float) -> void:
-	match current_state:
-		BattleState.PLAYER_TURN:
-			_handle_player_turn()
-		BattleState.ENEMY_TURN:
-			_handle_enemy_turn()
-		_:
-			pass
 
 func _transition_to_state(new_state: BattleState) -> void:
 	current_state = new_state
@@ -62,3 +54,21 @@ func _handle_enemy_turn() -> void:
 
 func _on_turn_ended() -> void:
 	print("Turn ended")
+
+
+func _on_state_changed() -> void:
+	match current_state:
+		BattleState.BATTLE_SETUP:
+			_setup_battle()
+		BattleState.PLAYER_TURN:
+			_handle_player_turn()
+		BattleState.PLAYER_CARD_PLAYING:
+			pass
+		BattleState.PLAYER_TURN_END:
+			pass
+		BattleState.ENEMY_TURN:
+			_handle_enemy_turn()
+		BattleState.ENEMY_TURN_END:
+			pass
+		BattleState.BATTLE_END:
+			pass
